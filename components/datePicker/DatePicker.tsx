a61bba893f7349ac7ab566d16329456c9ecb5a2a
lakshya <lakshyadahiya0246@gmail.com>
2023-10-15 16:59:15 -0400

    chore: datepicker update styles

--- components/datePicker/DatePicker.tsx
+++ components/datePicker/DatePicker.tsx
@@ -1,36 +1,7 @@
-import { addDays, format } from "date-fns";
-import { ComponentProps, useState } from "react";
-import { DateRange, DayPicker } from "react-day-picker";
+import React, { ComponentProps } from "react";
+import { DayPicker } from "react-day-picker";
 import "../../styles/datepicker.scss";
-import React from "react";
 
 export function DatePicker(props: ComponentProps<typeof DayPicker>) {
-  const defaultSelected: DateRange = {
-    from: new Date(),
-    to: addDays(new Date(), 4),
-  };
-  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
-
-  let footer = <p>Please pick the first day.</p>;
-  if (range?.from) {
-    if (!range.to) {
-      footer = <p>{format(range.from, "PPP")}</p>;
-    } else if (range.to) {
-      footer = (
-        <p>
-          {format(range.from, "PPP")}–{format(range.to, "PPP")}
-        </p>
-      );
-    }
-  }
-
-  return (
-    <DayPicker
-      {...props}
-      mode="range"
-      defaultMonth={new Date()}
-      selected={range}
-      onSelect={setRange}
-    />
-  );
+  return <DayPicker {...props} />;
 }
