441bdab4551df8f810576486950dca150dbdcb7f
lakshya <lakshyadahiya0246@gmail.com>
2023-10-28 20:18:13 -0400

    chore: update types - add updateItem function

--- nextjs/http/client.ts
+++ nextjs/http/client.ts
@@ -16,21 +16,25 @@ export const useMutation = <Payload extends any, Response = {}, Error = {}>(
   args?: Parameters<typeof useSWRMutation<Response, Error, string, Payload>>[2]
 ) => useSWRMutation<Response, Error, string, Payload>(route, fetcher, args);
 
-export type MutationFetcher<T = any> = (
-  url: string,
-  payload: { arg: T }
-) => Promise<T>;
-
-export const deleteItem: MutationFetcher<{
-  id: string;
-}> = async (url, payload) => {
-  const res = await fetch(`${url}`, {
+type MutationFetcherArgs<T = any> = [string, { arg: T }];
+export async function deleteItem(...args: MutationFetcherArgs<{ id: string }>) {
+  const res = await fetch(`${args[0]}`, {
     method: "DELETE",
     headers: {},
-    body: JSON.stringify(payload.arg),
+    body: JSON.stringify(args[1].arg),
   });
   return await (res.json() as Promise<{ id: string }>);
-};
+}
+
+export async function updateItem<T = any>(
+  ...args: MutationFetcherArgs<T>
+): Promise<T> {
+  return fetch(args[0], {
+    method: "PUT",
+    headers: {},
+    body: JSON.stringify(args[1].arg),
+  }).then((res) => res.json());
+}
 
 export async function addItem<T = any>(
   url: string,
